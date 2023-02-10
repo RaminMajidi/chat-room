1a266cb64829d26cc4f9b8970950bc39c9a6b92e
RaminMajidi <raminmajididev01@gmail.com>
2023-02-10 14:12:08 +0330

    Insert Meta Tags & Installed react-animation-on-scroll

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -6,9 +6,17 @@ const Layout = ({title,children,activeNavItem,classes})=>{
     return(
         <>
         <Head>
+            {/* <!-- start My meta --> */}
+            <meta name="robots" content="index, follow" />
+            <meta http-equiv="Cache-Control" content="no-cache" />
+            <meta name="designer" content="ramindev01" />
+            <meta name="author" content="frontEnd,Ramin Majidi,RaminDev01,رامین مجیدی" />
+            <meta name="owner" content="رامین مجیدی" />
+            <meta name="url" content="https://ramin-majidi.netlify.app" />
             <meta name="description" content="برنامه ساخته شده توسط رامین مجیدی به وسیله Next js" />
+            {/* <!-- end My meta --> */}
             <title>{`RaminMajidi || ${title}`}</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"/>        
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"/>     
         </Head>
         <Header activeNavItem={activeNavItem}/>
         <section className={`animate__animated animate__zoomIn w-full h-full mx-auto mb-[5rem] lg:mb-0 bg-transparent ${classes}`}>
