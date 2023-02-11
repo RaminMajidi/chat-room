5dcbf151a1bc77a0b7d57648fc48e3d763d3c410
RaminMajidi <raminmajididev01@gmail.com>
2023-02-11 13:50:56 +0330

    compeleted Single Portfolio Page

--- pages/Portfolio/[title].js
+++ pages/Portfolio/[title].js
@@ -5,6 +5,7 @@ import Layout from "../../components/layout/Layout"
 import NotItem from "../../components/design/NotItem/NotItem"
 import { useEffect, useState } from "react"
 import Loading from "../../components/design/Loading/Loading"
+import Link from "next/link"
 
 
 const PortfolioPage = (props)=>{
@@ -19,21 +20,30 @@ if(portfolioData){
         <Layout activeNavItem={"portfolio"} title={title}>
         {item ? (
            <section>
-           <h2 className="text-[var(--color-orange)] text-3xl text-center px-2 py-3">{item.title}</h2>
-           <Image src={item.coverImg} width={600} height={300} alt={item.title}/>
-           <article>
-            <div>
-            <i className='bx bxs-category'></i>
+           <Image src={item.coverImg} width={1000} height={500} alt={item.title} className="mx-auto my-3 p-2"/>
+           <article className=" w-full max-w-4xl mx-auto flex justify-start items-center flex-wrap p-2">
+           <div className="p-2 w-full h-10 flex items-center md:w-[50%] text-[var(--color-text)]">
+            <i className='bx bxl-sketch text-3xl text-red-600'></i>
+            <p className="mr-3 text-lg font-bold"><span> عنوان : </span><span>{item.title}</span></p>
             </div>
-            <div>
-            <i className='bx bxs-notepad'></i>
+            <div className="p-2 w-full h-10 flex items-center lg:w-[50%] text-[var(--color-text)]">
+            <i className='bx bxs-category text-3xl text-red-600'></i>
+            <p className="mr-3 text-lg font-bold"><span>دسته بندی : </span><span>{item.category}</span></p>
             </div>
-            <div>
-            <i className='bx bx-code-alt'></i>
+            <div className="p-2 w-full h-10 flex items-center md:w-[50%] text-[var(--color-text)]">
+            <i className='bx bx-code-alt text-3xl text-red-600'></i>
+            <p className="mr-3 text-lg font-bold"><span>زبانها : </span><span>{item.languages.length ? item.languages.join(' , ') :"فاقد آیتم!"}</span></p>
             </div>
-            <div>
-            <i className='bx bx-link-alt' ></i>
+            <div className="p-2 w-full h-10 flex items-center md:w-[50%] text-[var(--color-text)]">
+            <i className='bx bx-link-alt text-3xl text-red-600'></i>
+            <p className="mr-3 text-lg font-bold"><span>لینک برنامه : </span><Link className="text-left text-[var(--color-orange)]" href={item.url} target={"_blank"}>برای مشاهده کلیک کنید</Link></p>
             </div>
+            {item.packeages?.length ? (
+                <div className="p-2 w-full h-10 flex items-center text-[var(--color-text)]">
+                <i className='bx bxs-notepad text-3xl text-red-600'></i>
+                <p className="mr-3 text-lg font-bold"><span>وابستگیها : </span><span>{item.packeages.join(' , ')}</span></p>
+                </div>
+            ) : (null)}
            </article>
            </section>
         ) : (<NotItem/>)}
--- pages/Portfolio/index.js
+++ pages/Portfolio/index.js
@@ -43,12 +43,12 @@ filterCategory == "All" ? (setFilterData(data)) :(setFilterData(data.filter(item
             )) 
         }
         </div>
-        <div className=" overflow-hidden w-full flex flex-wrap justify-center max-w-7xl mx-auto my-2 px-2 py-4">
+        <div className=" overflow-hidden relative w-full flex flex-wrap justify-center max-w-7xl mx-auto my-2 px-2 py-4">
             {filterData.length ? (
                 filterData.map((item,index)=>(
                 <AnimationOnScroll key={index} duration={2} offset={230} initiallyVisible={index<3?true:false} animateIn="animate__bounceInLeft" animateOut="animate__bounceOutLeft">
-                <Link href={`/Portfolio/${item.title}`} key={item.title}>
-                <div key={item.id} className="group relative overflow-hidden">
+                <Link href={`/Portfolio/${item.title}`} key={item.title} className="">
+                <div key={item.id} className="group relative overflow-hidden ">
                     <div className="absolute bg-[var(--color-orange)] opacity-0 group-hover:opacity-100  duration-700 ease-in-out cursor-pointer right-0 flex justify-center items-center w-full h-56 md:max-w-[350px] lg:max-w-[400px] md:mx-2 my-4 rounded-2xl">
                         <h2 className="text-2xl text-slate-100 font-[Poppins]">{item.title}</h2>
                     </div>
