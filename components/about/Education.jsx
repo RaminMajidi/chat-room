b4246d77d048b11fe6577a3b7f7e95fc55455679
RaminMajidi <raminmajididev01@gmail.com>
2023-02-15 16:00:55 +0330

    debuged For Build Deploy

--- components/about/Education.jsx
+++ components/about/Education.jsx
@@ -8,7 +8,7 @@ const Education = ({data})=>{
          <h2 className='text-[var(--color-text)] font-bold text-center text-2xl p-1 mb-8'>تجربه و آموزش</h2>
          <div className=" grid grid-cols-12 gap-2 px-2 py-1">
             {data.map((item,index)=>(
-                <div className="flex h-auto min-h-[12rem]  col-span-12 md:col-span-6">
+                <div key={index} className="flex h-auto min-h-[12rem]  col-span-12 md:col-span-6">
                     <div className="relative h-full">
                     <i className='absolute z-10 top-0 right-0 w-8 h-8 rounded-full bg-[var(--color-orange)] text-center text-white text-xl bx bxs-briefcase'></i>
                     <span className="absolute top-1 right-[0.9rem] h-[90%] border border-[var(--color-orange)]"></span>
