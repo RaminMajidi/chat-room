0a8693b2470b54c65cd125dd7a6aadb7624dd018
RaminMajidi <raminmajididev01@gmail.com>
2023-02-15 17:59:57 +0330

    ####

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
-module.exports = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
-};
-
-
+module.exports = nextConfig
