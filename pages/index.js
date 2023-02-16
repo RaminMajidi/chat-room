785f3baefe2f4e17562166f426f2b537d87c2fd3
RaminMajidi <raminmajididev01@gmail.com>
2023-02-16 14:48:59 +0330

    Change Home Page Style

--- pages/index.js
+++ pages/index.js
@@ -26,14 +26,14 @@ const Home =(props)=> {
               <Layout title={"Home"} activeNavItem={"home"}>
               <div className='clip-path'></div>
               <section className='relative w-[100%] min-h-screen flex flex-col lg:flex-row-reverse justify-start items-center'>
-              <div className=' relative flex w-[220px] h-[220px] rounded-full border-8 border-solid border-[var(--color-main)] lg:rounded-3xl lg:pt-3  lg:border-none  bg-transparent lg:w-[30vw] lg:min-w-[250px] lg:ml-3 lg:h-[85vh] lg:shadow-[10px_15px_15px_0px_rgba(0,0,0,0.8)] mt-1'>
+              <div className=' relative flex w-[180px] h-[180px] rounded-full border-8 border-solid border-[var(--color-main)] lg:rounded-3xl lg:pt-3  lg:border-none  bg-transparent lg:w-[30vw] lg:min-w-[250px] lg:ml-3 lg:h-[85vh] lg:shadow-[10px_15px_15px_0px_rgba(0,0,0,0.8)] mt-1'>
                 <Image onLoadingComplete={(image)=>{image.classList.remove("opacity-0")}} alt='My-Image'  src={"/images/"+data.image} width={700} height={1000} className="opacity-0 bg-[#000] pt-2 rounded-full lg:rounded-3xl text-slate-200 "/>
               </div>
-              <div className='lg:ml-14 z-20 py-3 px-5'>
+              <div className='lg:ml-14 z-20 py-1 px-5'>
                 <h1 className='animate__animated animate__flash animate__delay-1s text-[var(--color-orange)] py-1 text-2xl lg:text-5xl md:text-4xl font-bold'>{data.title}</h1>
                 <h2 className='animate__animated animate__flash animate__delay-2s an font-[Poppins] text-[var(--color-gray)] py-1 text-lg lg:text-3xl md:text-2xl  mt-3 font-bold tracking-widest	'>{data.subTitle}</h2>
-                <p className='animate__animated animate__pulse animate__delay-4s   lg:inline-block lg:w-[50vw]  text-[var(--color-text)] py-1 text-justify leading-8 text-lg lg:text-xl lg:leading-10'>{data.desc}</p>
-              <BtnDownload classes={"animate__animated  animate__shakeY animate__delay-3s"}/>
+                <p className='animate__animated animate__pulse animate__delay-3s   lg:inline-block lg:w-[50vw]  text-[var(--color-text)] py-1 text-justify leading-8 text-lg lg:text-xl lg:leading-10'>{data.desc}</p>
+              <BtnDownload/>
               </div>
               </section>
             </Layout>
