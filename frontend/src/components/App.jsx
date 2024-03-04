ce181ae39ff59d27f17223bc01e96e093dc57c64
RaminMajidi <raminmajididev01@gmail.com>
2024-03-04 21:58:49 +0330

    created basic Login page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,3 +1,4 @@
+import Login from "../pages/login/Login"
 
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      
+      <Login/>
     </div>
   )
 }
