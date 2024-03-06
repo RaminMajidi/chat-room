75f98da60c9c7c27c2ab86095d15a338a3e8e47a
RaminMajidi <raminmajididev01@gmail.com>
2024-03-06 18:44:41 +0330

    created search user

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConvesation from "../../zustand/useConversation"
+import useGetConversation from "../../hooks/useGetConversation"
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+
+    const [search, setSearch] = useState('')
+    const { setSelectedConversation } = useConvesation()
+    const { conversations } = useGetConversation()
+
+    const submitHandler = async (e) => {
+        e.preventDefault()
+        if (!search) return;
+        if (search.length < 3) {
+            return toast.error('Search term must be at least 3 characters long !')
+        }
+        const conversation = conversations.find((c) =>
+            c.fullName.toLowerCase().includes(search.toLowerCase())
+        );
+        if (conversation) {
+            setSelectedConversation(conversation);
+            setSearch('')
+        } else {
+            toast.error("No such user found !")
+        }
+
+
+
+    }
+
     return (
-        <form className="flex items-center gap-2">
+        <form className="flex items-center gap-2" onSubmit={submitHandler}>
             <input
                 type="text"
                 placeholder="Search..."
                 className="input input-bordered rounded-full"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
             />
             <button
                 type="submit"
