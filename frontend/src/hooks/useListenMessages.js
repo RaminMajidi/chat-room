e37cc1f09f31f7fee9268cd976a7cf475594e3fa
RaminMajidi <raminmajididev01@gmail.com>
2024-09-15 23:28:32 +0330

    [remove] feature, send notification (frontend)

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -5,33 +5,28 @@ import notificationSound from "../assets/sounds/notification.mp3"
 import { useAuthContext } from '../context/AuthContext'
 import useGetConversation from './useGetConversation'
 import { messageAlert } from '../utils/Alerts'
-import { notifyMe,showNotification } from '../utils/Notification'
 
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
   const { messages, setMessages, selectedConversation } = useConversation();
   const { authUser } = useAuthContext();
-
-  const { loading, conversations } = useGetConversation();
+  const { conversations } = useGetConversation();
 
   useEffect(() => {
     socket?.on('newMessage', (newMessage) => {
       if (selectedConversation?._id === newMessage.senderId) {
         newMessage.shouldShake = true;
+        setMessages([...messages, newMessage]);
         const sound = new Audio(notificationSound);
         sound.play();
-        setMessages([...messages, newMessage]);
-        showNotification();
         return
       }
       if (newMessage.receiverId === authUser._id && selectedConversation?._id !== newMessage.senderId) {
         const sender = conversations.find(item => item._id === newMessage.senderId);
+        messageAlert(sender.fullName);
         const sound = new Audio(notificationSound);
         sound.play();
-        messageAlert(sender.fullName)
-        // notifyMe(sender.fullName,sender.profilePic);
-        showNotification();
         return
       }
     });
