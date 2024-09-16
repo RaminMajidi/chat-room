9fb7f0a3a0405fd1f00d89ff4a2ba83c2d60717b
RaminMajidi <raminmajididev01@gmail.com>
2024-09-16 23:58:44 +0330

    [change] responsive and styles (frontend)

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,6 @@
 import Sidebar from '../../components/sidebar/Sidebar'
 import MessageContainer from '../../components/messages/MessageContainer'
-
-//  sm:h-[450px] md:h-[550px] 
-//     
+  
 
 const Home = () => {
   return (
