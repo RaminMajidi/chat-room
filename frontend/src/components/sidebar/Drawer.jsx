6c0763e0e9eaefdc6099be1f9a2a4ac8c29f0493
RaminMajidi <raminmajididev01@gmail.com>
2024-09-19 00:54:40 +0330

    [change] responsive and styles (frontend)

--- frontend/src/components/sidebar/Drawer.jsx
+++ frontend/src/components/sidebar/Drawer.jsx
@@ -3,7 +3,7 @@ import Joint from './Joint'
 
 const Drawer = () => {
     return (
-        <div className="drawer drawer-end md:hidden z-20">
+        <div className="drawer drawer-end md:hidden z-50">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
                 {/* Page content here */}
